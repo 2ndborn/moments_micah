8b0f79307da9c7f5fc9f58d1e1fd6d1bab42edfa
2ndborn <micah.francis69@googlemail.com>
2025-04-27 14:17:50 +0100

    Added the sign in page and taliored the nav bar for no users

--- src/App.js
+++ src/App.js
@@ -4,20 +4,45 @@ import Container from 'react-bootstrap/Container'
 import { Route, Switch } from 'react-router-dom';
 import './api/axiosDefaults'
 import SignUpForm from './pages/auth/SignUpForm';
+import SignInForm from './pages/auth/SignInForm';
+import { createContext, useEffect, useState } from 'react';
+import axios from 'axios';
+
+export const CurrentUserContext = createContext()
+export const SetCurrentuserContext = createContext()
 
 function App() {
+const [currentUser, setCurrentUser] = useState(null)
+
+const handleMount = async () => {
+  try {
+    const {data} = await axios.get('/dj-rest-auth/user/')
+    setCurrentUser({data})
+  } catch(err) {
+    console.log(err)
+  }
+}
+
+useEffect(() => {
+  handleMount()
+}, [])
+
   return (
-    <div className={styles.app}>
-      <NavBar />
-      <Container className={styles.Main}>
-        <Switch>
-          <Route exact path="/" render={() => <h1>Home page</h1>} />
-          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />          
-          <Route exact path="/signup" render={() => <SignUpForm />} />          
-          <Route render={() => <h1>Page not found!</h1>} />          
-        </Switch>
-      </Container>
-    </div>
+    <CurrentUserContext value={currentUser}>
+      <SetCurrentuserContext value={setCurrentUser}>
+        <div className={styles.app}>
+          <NavBar />
+          <Container className={styles.Main}>
+            <Switch>
+              <Route exact path="/" render={() => <h1>Home page</h1>} />
+              <Route exact path="/signin" render={() => <SignInForm />} />
+              <Route exact path="/signup" render={() => <SignUpForm />} />
+              <Route render={() => <h1>Page not found!</h1>} />
+            </Switch>
+          </Container>
+        </div>
+      </SetCurrentuserContext>
+    </CurrentUserContext>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {Navbar, Nav, Container} from 'react-bootstrap';
 import logo from '../assets/logo.png';
 import styles from '../styles/NavBar.module.css'
 import { NavLink } from 'react-router-dom';
+import { CurrentUserContext } from '../App';
 
 const NavBar = () => {
+    const currentUser = useContext(CurrentUserContext)
+    const loggedOutIcons = (
+        <>
+            <NavLink to='/signin' 
+            className={styles.NavLink} 
+            activeClassName={styles.Active}
+            >
+                <i className='fas fa-sign-in-alt'></i>Sign in
+            </NavLink>
+            <NavLink to='/signup' 
+            className={styles.NavLink} 
+            activeClassName={styles.Active}
+            >
+                <i className='fas fa-user-plus'></i>Sign up
+            </NavLink>
+        </>
+    )
     return (
         <Navbar className={styles.NavBar} expand="md" fixed='top'>
             <Container>
