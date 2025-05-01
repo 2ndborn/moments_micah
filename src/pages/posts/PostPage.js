37262f3bc642b19b500301011c2b2a5164408961
2ndborn <micah.francis69@googlemail.com>
2025-05-01 19:07:57 +0100

    created the useEffect handle mount in post page

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
@@ -6,10 +6,23 @@ import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import {axiosReq} from '../../api/axiosDefaults'
 
 function PostPage() {
-    const { id } = useParams();
-    const [post, setPost] = useState({ resutls: [] });
+  const { id } = useParams();
+  const [post, setPost] = useState({ resutls: [] });
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [ {data: post}] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+        ])
+      } catch(err) {
+        console.log(err)
+      }
+    }
+  })
 
   return (
     <Row className="h-100">
