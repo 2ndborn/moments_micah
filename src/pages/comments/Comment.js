ee4458538a1c22300d6c7a1290d33441ea54051a
2ndborn <micah.francis69@googlemail.com>
2025-05-09 09:22:56 +0100

    Added the delete functionality to the handleDelete in comment.js

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -4,12 +4,43 @@ import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 import Avatar from '../../components/Avatar'
 import { useCurrentUser } from '../../contexts/CurrentUserContext'
 import { MoreDropdown } from '../../components/MoreDropdown'
+import { axiosRes } from '../../api/axiosDefaults'
 
 const Comment = (props) => {
-    const { profile_id, profile_image, owner, updated_at, content } = props
+    const {
+        profile_id,
+        profile_image,
+        owner,
+        updated_at,
+        content,
+        id,
+        setPost,
+        setComments,
+    } = props
 
     const currentUser = useCurrentUser()
     const is_owner = currentUser?.username === owner
+
+    const handleDelete = async () => {
+        try {
+            await axiosRes.delete(`/comments/${id}/`)
+            setPost((prevPost) => ({
+                results: [
+                    {
+                        ...prevPost.results[0],
+                        comments_count: prevPost.results[0].comments_count - 1
+                    }
+                ]
+            }))
+
+            setComments((prevComments) => ({
+                ...prevComments,
+                results: prevComments.results.filter((comment) => comment.id !== id)
+            }))
+        } catch(err) {
+            console.log(err)
+        }
+    }
     
     return (
         <div>
@@ -23,7 +54,7 @@ const Comment = (props) => {
                     <span className={styles.Date}>{updated_at}</span>
                     <p className="mt-1 mb-0">{content}</p>
                 </div>
-                {is_owner && <MoreDropdown handleEdit={()=>{}} handleDelete={()=>{}} />}
+                {is_owner && <MoreDropdown handleEdit={()=>{}} handleDelete={handleDelete} />}
             </div>
         </div>
     )
