4309b649ac9f5800d8e383ac0c101c62f694a324
2ndborn <micah.francis69@googlemail.com>
2025-05-09 18:39:25 +0100

    Displayed the popular profiles

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,13 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import appStyles from '../../App.module.css'
 import { Container } from 'react-bootstrap'
+import { axiosReq } from '../../api/axiosDefaults'
+import { useCurrentUser } from '../../contexts/CurrentUserContext'
+import Asset from '../../components/Asset'
 
 const PopularProfiles = () => {
-  return (
-    <Container className={appStyles.Content}>
-        <p>Most followed profiles</p>
-    </Container>
-  )
+    const [profileData, setProfileData] = useState({
+        // We will use for our profile page later!
+        pageProfile: { results: [] },
+        popularProfiles: { results: [] }
+    })
+
+    const { popularProfiles } = profileData;
+    const currentUser = useCurrentUser();
+
+    useEffect(() => {
+        const handleMount = async () => {
+            try {
+                const { data } = await axiosReq.get(
+                    '/profiles/?ordering=-followers_count'
+                )
+                console.log(data)
+                setProfileData((prevState) => ({
+                    ...prevState,
+                    popularProfiles: {results: data.results},
+                }))
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        handleMount()
+    }, [currentUser])
+
+    return (
+        <Container className={appStyles.Content}>
+            <p>Most followed profiles</p>
+            {popularProfiles.results.length ? (
+                <>
+                    {popularProfiles.results.map((profile) => (
+                        <p key={profile.id}>{profile.owner}</p>
+                    ))}
+                </>
+            ) : (
+                <Asset spinner />
+            )}
+        </Container>
+    )
 }
 
 export default PopularProfiles
