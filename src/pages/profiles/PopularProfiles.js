999c33e217d9f80dc4437216e8db4cb7f9ccb8db
2ndborn <micah.francis69@googlemail.com>
2025-05-21 06:06:01 +0100

    optimized bootstrap on the popular profiles page

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import appStyles from '../../App.module.css'
-import { Container } from 'react-bootstrap'
+import Container from 'react-bootstrap'
 import Asset from '../../components/Asset'
 import { useProfileData } from '../../contexts/ProfileDataContext'
 import Profile from './Profile'
